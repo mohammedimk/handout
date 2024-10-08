6f906bea65b8c6d20c06223b5711dea312bf8c9f
unknown <mkusada49@gmail.com>
2024-10-08 14:47:43 +0100

    nkjjj

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os.path
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k@9du^fm@ofj#m%zmqb725=er6ga!5gfq!ty@j8#js2)dhhj54'
-
+#SECRET_KEY = 'django-insecure-k@9du^fm@ofj#m%zmqb725=er6ga!5gfq!ty@j8#js2)dhhj54'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = DEBUG_VALUE
 
 ALLOWED_HOSTS = ["bokoapp1-f0d69d252fb4.herokuapp.com","127.0.0.1"]
 
@@ -131,3 +132,4 @@ STATICFILES_DIRS=[os.path.join(BASE_DIR, "static")]
 MEDIA_URL="/media/"
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media_cdn')
 
+django_heroku.settings(locals())
