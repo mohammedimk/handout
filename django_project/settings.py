ab16fbbc974011238504157e5a737342e36a5b3f
unknown <mkusada49@gmail.com>
2024-10-09 12:16:15 +0100

    static

--- django_project/settings.py
+++ django_project/settings.py
@@ -128,7 +128,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL='/static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR, "static")]
+# STATICFILES_DIRS=[os.path.join(BASE_DIR, "static")]
 MEDIA_URL="/media/"
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media_cdn')
 
